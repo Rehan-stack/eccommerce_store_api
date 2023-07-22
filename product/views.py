a2e000281d78b29d03ea7e3a288f697cb2ab4112
Rehan Ahmed <73068765+Rehan-stack@users.noreply.github.com>
2023-07-22 11:24:20 +0500

    UPDATE1

--- product/views.py
+++ product/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from rest_framework.generics import ListAPIView,CreateAPIView,RetrieveUpdateDestroyAPIView
+from .serializers import *
+from .models import *
+from rest_framework.response import Response
 # Create your views here.
+
+class List_product(ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = Product_serializer
+
+
+class list_product_details(ListAPIView):
+    serializer_class = Product_detail_serializer
+
+    def get_queryset(self):
+        pk = self.kwargs['pk']
+        return Product_details.objects.filter(product = pk)
+    
+
+
