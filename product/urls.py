8ee008770d45b4079c7fc87e4fafb5df817e2eb8
Rehan Ahmed <73068765+Rehan-stack@users.noreply.github.com>
2023-07-23 23:10:11 +0500

    new2

--- product/urls.py
+++ product/urls.py
@@ -4,5 +4,6 @@ from .views import *
 
 urlpatterns = [
     path('list', List_product.as_view()),
-    path('list/<int:pk>',list_product_details.as_view())
+    path('list/<int:pk>',list_product_details.as_view()),
+    path('create/<int:pk>',create_product.as_view()),
 ]
--- product/views.py
+++ product/views.py
@@ -3,19 +3,35 @@ from rest_framework.generics import ListAPIView,CreateAPIView,RetrieveUpdateDest
 from .serializers import *
 from .models import *
 from rest_framework.response import Response
+from rest_framework.validators import ValidationError
 # Create your views here.
-
+ 
 class List_product(ListAPIView):
     queryset = Product.objects.all()
     serializer_class = Product_serializer
 
-
+ 
 class list_product_details(ListAPIView):
     serializer_class = Product_detail_serializer
-
+      
     def get_queryset(self):
         pk = self.kwargs['pk']
         return Product_details.objects.filter(product = pk)
     
 
 
+class create_product(CreateAPIView):
+    serializer_class = Product_serializer
+
+    def perform_create(self, serializer):
+        pk = self.kwargs['pk']
+
+        query = Product.objects.get(pk=pk)
+        
+
+        add_product = Product_details.objects.filter(query=query)
+        if add_product.exists():
+            raise ValidationError('product already exists, try to add new product.')
+        else:
+            query.save()
+        return Response(query.data)
